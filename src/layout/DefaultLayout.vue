6c6548464cc098dc1545f8d5dbd61fedd756ce41
Vict Christine De Asis <77196423+vcrstn@users.noreply.github.com>
2026-09-15 13:43:08 +0800

    Add v-main and router-view to DefaultLayout

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -2,13 +2,17 @@
 <v-app>
     <Header />
     <NavigationDrawer />
-</v-app>
 
+    <v-main>
+        <router-view />
+    </v-main>
+</v-app>
 </template>
 
 <script setup lang="ts">
 import Header from '@/components/layout/Header.vue'
 import NavigationDrawer from '@/components/layout/NavigationDrawer.vue';
+//import Footer from '@/components/layout/Footer.vue'
 </script>
 
  
